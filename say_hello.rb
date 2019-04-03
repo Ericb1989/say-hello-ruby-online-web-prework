b97b140316b3d9a03cf0314abf79e9936ec1e5b6
Eric Bodden <ericbodden30@gmail.com>
2019-04-03 20:49:16 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,5 @@
-# Build your say_hello method here
+# I call on the method say_hello, and give it the string {"Maeve"}
+say_hello ("Maeve")
+ # The method prints this text to the screen:
+Hello Maeve!
+end
